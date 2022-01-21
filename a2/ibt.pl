3937eaa964f912180d7df9e58903e6fcc61c0503
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-01-21 18:09:45 +0530

    Worked on a few more rules

--- a2/ibt.pl
+++ a2/ibt.pl
@@ -27,3 +27,39 @@ postorder(node(N,L,R), T) :- postorder(L,PL), postorder(R,PR), append(PL, PR, K)
 
 inorder(empty,[]).
 inorder(node(N,L,R), T) :- inorder(L,PL), inorder(R,PR), append([N], PR, K), append(PL, K, T).
+
+toString(empty,"()").
+toString(node(N,L,R), S) :- toString(L, Lstr), toString(R, Rstr), 
+                            number_string(N, Nstr), 
+                            string_concat("(", Nstr, S1),
+                            string_concat(S1, ",", S2),
+                            string_concat(S2, Lstr, S3),
+                            string_concat(S3, ",", S4),
+                            string_concat(S4, Rstr, S5),
+                            string_concat(S5, ")", S).
+isBalanced(empty).
+isBalanced(node(_,L,R)) :- isBalanced(L), isBalanced(R), height(L,Lh), height(R, Rh), abs(Lh-Rh) < 2.
+
+allNodes(empty,[]).
+allNodes(node(N,L,R), T) :- allNodes(L, Ln), allNodes(R, Rn), append(Ln,Rn,T1), T = [N|T1].
+
+allLessThan([],_).
+allLessThan([V|L],N) :- V < N, allLessThan(L,N).
+
+allGreaterThan([],_).
+allGreaterThan([V|L],N) :- V > N, allGreaterThan(L,N).
+
+isBST(empty).
+isBST(node(N,L,R)) :- allNodes(L,Ln), allNodes(R,Rn), allLessThan(Ln,N), allGreaterThan(Rn,N).
+
+/* TODO think about this
+makeBST([],_).
+makeBST([A|L],node(N,L,R)) :- 
+
+make this work in conjunction with
+insert(N, BST1, BST2) 
+delete(N, BST1, BST2)
+*/
+
+lookup(_, empty) :- false.
+lookup(N, node(Root,L,R)) :- (N == Root); ((N < Root) -> lookup(N,L); lookup(N,R)).
